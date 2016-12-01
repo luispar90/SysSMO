656663f8cae04f25580f6b4556f0661ff9803a35
lparagua <lparagua@LIM-6KF7KC2.usersad.everis.int>
2016-12-01 11:41:13 -0500

    Cambios menores

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://192.168.100.38:8081/SysSMO/';
+$config['base_url'] = 'http://192.168.100.69:8081/SysSMO/';
 
 /*
 |--------------------------------------------------------------------------
